1ce68b3d601f9529a57d336246465613646ed00f
nicholasbair <bairinc@gmail.com>
2017-06-24 17:23:42 -0400

    Feature spec passing.

--- spec/features/category_feature_spec.rb
+++ spec/features/category_feature_spec.rb
@@ -27,7 +27,7 @@ describe 'Feature Test: Category', :type => :feature do
 
     context "logged in" do
       before(:each) do
-        @user = User.first
+        @user = User.create(:email => 'test@test.com', :password => 'password')
         login_as(@user, scope: :user)
       end
 
